534c501e63b0e5cac6db97b682692b52e3931913
Wayne Daniels <wa.daniels@hotmail.com>
2020-01-22 06:19:49 -0800

    Memory Match Flip Cards Feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,8 @@ var clickCard = document.getElementById("gameCards");
 clickCard.addEventListener('click', handleClick);
 function handleClick(event) {
   console.log(event);
+  if (event.target.className.indexOf("card-back") === -1) {
+    return;
+  }
+  event.target.className += "hidden";
 }
